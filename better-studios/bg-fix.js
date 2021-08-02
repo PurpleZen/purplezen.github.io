7f7615393e6eb3992d623eb580b56d08e5bfe00a
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-08-02 16:37:05 -0700

    Update bg-fix.js

--- better-studios/bg-fix.js
+++ better-studios/bg-fix.js
@@ -1,1 +1,2 @@
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-page { background-color: #fcfcfc}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".replies.collapsed > .comment:last-of-type:after { background: none}"})));
