c374bc09326b49fb31ec3d6cbad4be7980d6cda8
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-08-02 14:35:29 -0700

    Update code.js

--- better-studios/code.js
+++ better-studios/code.js
@@ -3,3 +3,7 @@ void(document.head.appendChild(Object.assign(document.createElement('style'), { 
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { width: 50%}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { padding-bottom: 11px}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { padding-top: 11px}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".button { padding: 0.5em 1em}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-title { font-size: 25px}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-title { font-weight: 500}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-tab-nav li:active { padding: 0.5em 1em}"})));
