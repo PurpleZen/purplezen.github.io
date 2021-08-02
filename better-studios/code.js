dbee71583c1f0e428f5d17561c30279e8d8a2e55
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-08-02 13:57:18 -0700

    Update code.js

--- better-studios/code.js
+++ better-studios/code.js
@@ -1,2 +1,2 @@
-void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: '.studio-page { background-color: #fcfcfc}'})));
-void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-tab-nav { font-size: 11px}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-page { background-color: #fcfcfc}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-tab-nav li { padding: 0.5em 1.25em 0.5em 1em}"})));
