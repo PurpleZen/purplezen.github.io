20105201e28b5e8fdc086aed7a3390b2d1a7bd95
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-08-02 19:37:51 -0700

    Update full-fix.js

--- better-studios/full-fix.js
+++ better-studios/full-fix.js
@@ -1,8 +1,9 @@
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-page { background-color: #fcfcfc}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-tab-nav li { padding: 0.5em 1.25em 0.5em 1em}"})));
-void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { width: 50%}"})));
-void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { padding-bottom: 11px}"})));
-void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { padding-top: 11px}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { width: 40%}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { padding-bottom: 7px}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { padding-top: 7px}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-follow-button { font-size: 12px}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".button { padding: 0.5em 1em}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-title { font-size: 25px}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-title { font-weight: 500}"})));
@@ -12,5 +13,3 @@ void(document.head.appendChild(Object.assign(document.createElement('style'), { 
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info { gap: 10px}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".sub-nav li, .sub-nav button { border-radius: 12px}"})));
 void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-tab-nav li { background: transparent}"})));
-
-
