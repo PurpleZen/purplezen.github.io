aa0fc500153130f232082d09c6fae9fe319e776e
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-08-06 12:52:09 -0700

    Create mobile-fix.js

---
+++ better-studios/mobile-fix.js
@@ -0,0 +1,2 @@
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-description { height: 12rem}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".flex-row { flex-direction: initial}"})));
