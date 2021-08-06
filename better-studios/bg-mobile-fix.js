d9a1af1ab244d091e37f5bfb5091bb93c1dcd1a6
Leo <87787046+LegoManiac04@users.noreply.github.com>
2021-08-06 12:56:21 -0700

    Create bg-mobile-fix.js

---
+++ better-studios/bg-mobile-fix.js
@@ -0,0 +1,4 @@
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-page { background-color: #fcfcfc}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".replies.collapsed > .comment:last-of-type:after { background: none}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".studio-info .studio-description { height: 12rem}"})));
+void(document.head.appendChild(Object.assign(document.createElement('style'), { textContent: ".flex-row { flex-direction: initial}"})));
